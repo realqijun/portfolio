89be93fb5282b513a45087f6de12dff79e937c22
Qi Jun Ang <qang@u81z06s02.42kl.edu.my>
2024-07-24 16:38:24 +0800

    add stuff

--- helloworld.js
+++ helloworld.js
@@ -1,11 +1,18 @@
-let i = 0;
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
 
-function fib(n)
-{
-    if (n == 0 || n == 1)
-        return (1);
-    else
-        return (fib(n - 1) + fib(n - 2));
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
-
-console.log(fib(i));

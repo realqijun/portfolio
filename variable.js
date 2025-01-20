e15fa2c14f60aeaef4de6f5204809c2853c8c7b0
Theodore <ang.qijun2304@gmail.com>
2025-01-20 17:15:59 +0800

    Initial commit

--- variable.js
+++
@@ -1,8 +0,0 @@
-let string = "hello"
-let boolean = false
-let undef = undefined
-let nal = null
-console.log(typeof string)
-console.log(typeof boolean)
-console.log(typeof undef)
-console.log(typeof nal)

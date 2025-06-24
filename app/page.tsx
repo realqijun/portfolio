4066d86ddaf6c3c0574f436788ba2546b3d056b7
realqijun <ang.qijun2304@gmail.com>
2025-06-24 19:12:26 +0800

    Add finished website

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
-import Header from "./components/Header";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import Process from "@/components/Process";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+    <main className="relative bg-black flex justify-center overflow-clip items-center flex-col mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems}/>
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Process />
+        <Footer />
+      </div>
+    </main>
   );
 }
